3f25c508748284a8213306b2ecd4e7df9b5447e3
marqueserick <erickmarques43@gmail.com>
2022-08-30 16:41:26 -0300

    corrige parametros

--- src/TestaInsertComParametro.java
+++ src/TestaInsertComParametro.java
@@ -8,8 +8,8 @@ public class TestaInsertComParametro {
             String sql = "INSERT INTO PRODUTO (NOME, DESCRICAO) VALUES (?,?)";
 
             try (PreparedStatement st = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
-                adicionarVariavel("TELEVISÃO", "SMARTTV 50 POLEGADAS ANDROID TV", con, st);
-                adicionarVariavel("TABLET", "XIAOMI MI PAD 5 PRO", con, st);
+                adicionarVariavel("TELEVISÃO", "SMARTTV 50 POLEGADAS ANDROID TV", st);
+                adicionarVariavel("TABLET", "XIAOMI MI PAD 5 PRO", st);
 
                 con.commit();
             } catch (Exception e) {
@@ -20,7 +20,7 @@ public class TestaInsertComParametro {
         }
     }
 
-    private static void adicionarVariavel(String nome, String descricao, Connection con, PreparedStatement st) throws SQLException {
+    private static void adicionarVariavel(String nome, String descricao, PreparedStatement st) throws SQLException {
         st.setString(1, nome);
         st.setString(2, descricao);
         //if(nome.equals("TABLET")) throw new RuntimeException("Não pode adicionar tablet");
